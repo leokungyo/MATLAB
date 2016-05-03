6f78cb9e670983dffa3f523b851ab4cae9d81d37
QingYu <spencerliuqingyu993@gmail.com>
2016-05-03 16:27:46 +0800

    fix error

--- octree_test.m
+++ octree_test.m
@@ -6,6 +6,6 @@ cols = lines(OT.BinCount);
 doplot3 = @(p,varargin)plot3(p(:,1),p(:,2),p(:,3),varargin{:});
 for i = 1:OT.BinCount
     set(boxH(i),'Color',cols(i,:),'LineWidth', 1+OT.BinDepths(i))
-    doplot3(pts(OT.PointBins==i,:),'.','Color',cols(i,:))
+    doplot3(trex(OT.PointBins==i,:),'.','Color',cols(i,:))
 end
 axis image, view(3)
