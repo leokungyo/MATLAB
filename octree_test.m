0ecfaf7b8030c0cfc203bcb381b2ee3097149365
QingYu <spencerliuqingyu993@gmail.com>
2016-05-03 15:57:51 +0800

    update OCTree compression

---
+++ octree_test.m
@@ -0,0 +1,11 @@
+load trex.dat
+OT = OcTree(trex,'binCapacity',20);        
+figure
+boxH = OT.plot;
+cols = lines(OT.BinCount);
+doplot3 = @(p,varargin)plot3(p(:,1),p(:,2),p(:,3),varargin{:});
+for i = 1:OT.BinCount
+    set(boxH(i),'Color',cols(i,:),'LineWidth', 1+OT.BinDepths(i))
+    doplot3(pts(OT.PointBins==i,:),'.','Color',cols(i,:))
+end
+axis image, view(3)
