5a916cc7adddc843632af0858c64866da82638bb
QingYu <spencerliuqingyu993@gmail.com>
2016-05-08 17:03:38 +0800

    predictive update

--- predictive.m
+++ predictive.m
@@ -1,70 +1,50 @@
 clear all
 close all
 clc
-%Read the input image
-% A=imread('rice.png');
-% figure,imshow(A);
-% A=double(A);
 
 load trex.dat;
 trex = trex';
-e = trex;%error array for predictive coding
+e_trex = trex;%error array for predictive coding
 
 %Perform prediction error
 for i = 1:size(trex,1)
     for j = 2:size(trex,2)
-        e(i,j)=e(i,j)-trex(i,j-1);
+        e_trex(i,j)=e_trex(i,j)-trex(i,j-1);
     end
 end
 %display(e);
 
-%Huffman coding
-% C=reshape(e,[],1);
-% %quantization, otherwise huffman won't have that many codes
-% C = 8 * C;
-% for i=1:size(C)
-%     C(i) = vpa(C(i),2);
-% end
-% C = C/8;
-% 
-% [D1,x]=hist(C,min(min(e)):max(max(e)));
-% sym=x(D1>0);
-% prob=D1(D1>0)/numel(e);
-% [dict,avglen] = huffmandict(sym,prob);
-% comp = huffmanenco(C,dict);
-% 
-% 
-% %Huffman Decoding
-% dsig = huffmandeco(comp,dict);
-% e=reshape(dsig,size(trex,1),size(trex,2));
-% d=e;
-% 
-e = e';
-size(e,1)
-for i =1:size(e,1)
-    e(i,1) = round(e(i,1),2);
-    e(i,2) = round(e(i,2),2);
-    e(i,3) = round(e(i,3),2);
+e_trex = e_trex';
+%quantization
+for i =1:size(e_trex,1)
+    e_trex(i,1) = round(e_trex(i,1),2);
+    e_trex(i,2) = round(e_trex(i,2),2);
+    e_trex(i,3) = round(e_trex(i,3),2);
 end
-e = e';
+e_trex = e_trex';
 
-trex_revert = e;
+%revert to get the picture
+trex_revert = e_trex;
 for i = 1:size(trex,1)
     for j = 2:size(trex,2)
-        trex_revert(i,j)=trex_revert(i,j-1)+e(i,j);
+        trex_revert(i,j)=trex_revert(i,j-1)+e_trex(i,j);
     end
 end
 trex = trex';
-e = e';
+e_trex = e_trex';
 trex_revert = trex_revert';
+
+%plot the figure out
 figure;scatter3(trex(:,1),trex(:,2),trex(:,3),'.');axis equal;
 figure;scatter3(trex_revert(:,1),trex_revert(:,2),trex_revert(:,3),'.');axis equal;
 
+% write the origin data and compressed data into files in the same way
+% to compare the file size
 csvwrite('predict_before.dat',trex);
-csvwrite('predict_after.dat',e);
+csvwrite('predict_after.dat',e_trex);
 before = dir('predict_before.dat');
 after = dir('predict_after.dat');
-compression_rario = after.bytes/before.bytes;
-MSE_predict = immse(trex,trex_revert);
-% %display(d);
+%compress ratio and MSE
+trex_compression_rario_predict = after.bytes/before.bytes;
+trex_MSE_predict = immse(trex,trex_revert);
 
